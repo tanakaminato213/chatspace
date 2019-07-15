916b68c6b1ce93fcf1045b2bea79c7fe23af3784
tanakaminato213 <qwsazx012345@icloud.com>
2019-07-15 20:56:48 +0900

    commmit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'tanakaminato'
-  config.asset_host = 'https://s3-ap-northeast-1/tanakaminato'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tanakaminato'
 end
